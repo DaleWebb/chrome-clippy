e813bc7c1b1eb356954cb4d47b123e51905687fb
Dale Alexander Webb <dwebb@alacrityfoundation.com>
2014-03-04 20:34:39 +0000

    added the rest of the clippy assets

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,6 +1,4 @@
-document.addEventListener("DOMContentLoaded", function() {
-  var h1 = document.getElementsByTagName('h1');
-  if (h1.length > 0) {
-    h1[0].innerText = h1[0].innerText + ' \'Allo';
-  }
-}, false);
+clippy.load('Merlin', function(agent){
+	// do anything with the loaded agent
+	agent.show();
+});
